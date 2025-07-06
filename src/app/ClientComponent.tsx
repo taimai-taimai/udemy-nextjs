5506f12eee2efbd3dd5b2b5a8a1d23e519eb060d
taimai-taimai <204948513+taimai-taimai@users.noreply.github.com>
2025-07-06 21:00:50 +0900

    routehandler

--- src/app/ClientComponent.tsx
+++ src/app/ClientComponent.tsx
@@ -4,11 +4,32 @@ import React from "react";
 import { useState } from "react";
 
 const ClientComponent = () => {
-    const [count, setCount] = useState(0);
+    // 単一のユーザーオブジェクトとして管理
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+    
+    const onChangeData = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    const handleSubmit = async () => {
+        fetch("/api/users", {method: "POST",body: JSON.stringify(user),}).then((response) => {
+            response.json().then((res) => console.log(res))
+        })
+    }
+    
     return (
         <div>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <input className="border" type="text" name="name" value={user.name} onChange={onChangeData} />
+            <input className="border" type="text" name="email" value={user.email} onChange={onChangeData} />
+            <input className="border" type="text" name="password" value={user.password} onChange={onChangeData} />
+            <button onClick={handleSubmit}>送信</button>
         </div>
     )
 }
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,29 @@
+import prisma from "../../../../lib/prisma"
+import { z } from "zod"
+
+const userSchema = z.object({
+    name: z.string().min(1).max(30),
+    email: z.string().email().max(30),
+    password: z.string().min(8).max(100),
+})
+
+export async function POST(request: Request) {
+    const res = await request.json()
+
+    const validation = userSchema.safeParse(res)
+
+    if (!validation.success) {
+        return Response.json({ error: validation.error.message }, { status: 400 })
+    }
+
+    const { name, email, password } = validation.data
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password,
+        }
+    })
+    return Response.json({ user })
+  }
