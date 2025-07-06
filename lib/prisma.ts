654a41e4f2948e07702f2bfcadd3005c2dd1458f
taimai-taimai <204948513+taimai-taimai@users.noreply.github.com>
2025-07-06 20:10:30 +0900

    prisma設定変更

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma'
+import { PrismaClient } from './prisma/generated'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 type PrismaClientWithExtensions = ReturnType<typeof createPrismaClient>
